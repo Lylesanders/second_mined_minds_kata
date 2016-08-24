5c7bb155806c40a8c2f623e14a38fd0d278231ce
lyle Sanders <lylesanders66@yahoo.com>
2016-08-24 17:22:46 -0400

    to correct error in test

--- test_mined_minds_function_2.rb
+++ test_mined_minds_function_2.rb
@@ -5,9 +5,8 @@ require_relative "mined_minds_function_2.rb"	# This points minitest at the corre
 
  class TestMinedMindsFunction <Minitest::Test 	# this creates a class which is a template that passes real values instead of variables, hard coded values.
  	def test_1_equals_1							# This test send the value 1 to mined_minds_function_w and checks if the function returns 1
- 		assert_equal(1,1)
+ 		assert_equal(1, mined_minds(1))
  	end
-
 
 
  end	#this end is for the class
